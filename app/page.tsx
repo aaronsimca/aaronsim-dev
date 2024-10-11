c3b09ba0b3c90191e6b204a2b5954c7dde194917
aaronsimca <aaronsimhq@gmail.com>
2024-10-11 12:02:08 -0700

    Changing the Book Link

--- app/page.tsx
+++ app/page.tsx
@@ -180,6 +180,7 @@ export default function Home() {
   }
 
   return (
+    
     <div className="w-full h-screen bg-cover bg-center relative overflow-hidden" style={{ backgroundImage: "url('https://i.imgur.com/TgqaYRH.jpg')" }}>
       {/* Desktop Icons */}
       <div className="absolute top-1 left-5 grid grid-cols-[repeat(auto-fill,minmax(80px,1fr))] grid-rows-[repeat(auto-fill,minmax(80px,1fr))] max-h-[calc(100vh-2rem)] max-w-[240px] overflow-y-auto gap-1">
@@ -189,7 +190,7 @@ export default function Home() {
         <DesktopIcon label="Notepad" icon="📝" onClick={handleNotepadIconClick} />
         <DesktopIcon label="Twitter" icon="𝕏" onClick={() => window.open('https://twitter.com/aaronsiim', '_blank')} />
         <DesktopIcon label="GitHub" icon="💻" onClick={() => window.open('https://github.com/aaronsimca/', '_blank')} />
-        <DesktopIcon label="Book" icon="📕" onClick={() => window.open('https://aaronsim.gumroad.com/l/web3cryptotoolkit', '_blank')} />
+        <DesktopIcon label="Book" icon="📕" onClick={() => window.open('https://a.co/d/ac8QMF4', '_blank')} />
         <DesktopIcon label="LinkedIn" icon="📋" onClick={() => window.open('https://www.linkedin.com/in/aaronsimca/', '_blank')} />
       </div>
 
